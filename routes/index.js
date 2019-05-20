370174a7c79213df1921f76e5dbc0a9576927bdb
david1488tv <pogl787898@gmail.com>
2019-05-21 00:49:52 +0300

    add seeds

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 
 const authRouter = require(base_dir + '/app/http/controllers/auth');
 const accountRouter = require(base_dir + '/app/http/controllers/account');
-const adminConferencesRouter = require(base_dir + '/app/http/controllers/admin-conference');
-const adminTalksRouter = require(base_dir + '/app/http/controllers/admin-talk');
+const adminConferencesRouter = require(base_dir + '/app/http/controllers/admin/conference');
+const adminTalksRouter = require(base_dir + '/app/http/controllers/admin/talk');
 
 const authMiddleware = require(base_dir + '/app/http/middleware/authMiddleware');
 
