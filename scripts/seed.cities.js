a0836c056873bb812785dc3b1bbc4f4684fed3f6
david1488tv <pogl787898@gmail.com>
2019-05-21 20:29:35 +0300

    update cities seeds

--- scripts/seed.cities.js
+++ scripts/seed.cities.js
@@ -4,21 +4,20 @@ global.base_dir = './../diplom';
 const mongoose = require('mongoose');
 const config = require('./../config');
 
-mongoose.connect(config.database.dbUrl, { useMongoClient: true });
+mongoose.connect(config.database.dbUrl, {useMongoClient: true});
 
 console.log('================== seeds start ==================');
 
 const cities = require('./../seeds/cities');
 const City = require('./../app/models/city');
-async function seed () {
+
+async function seed() {
     for (let i in cities) {
-        for(let j in cities[i].cities) {
-            console.log(cities[i].cities[j].name)
-            await City.create({
-                name:  cities[i].cities[j].name,
-                description: cities[i].name
-            });
-        }
+        console.log(cities[i])
+        await City.create({
+            name: cities[i].city,
+            description: cities[i].admin
+        });
     }
 
     console.log('================== seeds complete ==================');
